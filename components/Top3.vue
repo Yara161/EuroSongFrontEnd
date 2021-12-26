252a568215091c0a4d98b4c995f62fa0e28e50ed
Yara161 <66169990+Yara161@users.noreply.github.com>
2021-12-26 16:39:08 +0100

    Add files via upload

---
+++ components/Top3.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+       
+        <button @click="changeIndex(-1)">
+            Prev song
+        </button>
+
+        <div v-for="(song,index) in items" :key="song.id">
+            <div v-if="index == activeIndex">
+
+            {{ song.artist.name}} - {{ song.title }}
+            <iframe :src="song.spotify" width="100%" height="50%" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            </div>
+        </div>
+
+        <button @click="changeIndex(+1)">
+            Next song
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Top3Songs",
+        props: [
+            "items",
+            
+        ],
+        methods:{
+           
+        }
+    }
+</script>
