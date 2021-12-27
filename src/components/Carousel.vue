35f2303b22dca1e5373ea5b9c8c1d57263dd2638
Yara161 <66169990+Yara161@users.noreply.github.com>
2021-12-27 10:45:22 +0100

    Add files via upload

---
+++ src/components/Carousel.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+       
+       
+    <div class="songsgame">
+         <img  @click="changeIndex(+1)" class="arrow" src="../assets/arrowleft.png">
+
+        <div  v-for="(song,index) in items" :key="song.id">
+            <div v-if="index == activeIndex">
+
+            {{ song.artist.name}} - {{ song.title }}
+            <iframe class="song"  :src="song.spotify" width="100%" height="100%" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            </div>
+        </div> 
+        <img  @click="changeIndex(+1)" class="arrow" src="../assets/arrowright.png"> 
+
+    </div>
+
+      
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Carousel",
+        props: [
+            "items",
+            "activeIndex"
+        ],
+        methods:{
+            changeIndex(value) {
+                //copy, you cannot change props directly
+                let index = this.activeIndex;
+
+                //-1 of 1
+
+                index +=value;
+                //check if index is the beginning or ending
+                if (index<0){
+                    index= this.items.length-1;
+                }
+
+                else if( index == this.items.length)
+                {
+                    index=0;
+                }
+                //change index
+                this.$emit("change-index", index);               
+                console.log(index);
+            }
+        }
+    }
+</script>
---
+++ src/pages/Homepage.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+         <Navigation @change-page="goToPage"/>
+  
+        <h1 class="title"> Eurosong voting app </h1>
+        <h2 class="title2">
+           Total songs: {{songs.length}}
+        </h2>
+    </div>
+</template>
+<script>
+import Navigation from "..//components/Navigation.vue"
+export default {
+    name:"Homepage",
+    components: {
+        Navigation
+    },
+    data(){
+        return{ 
+            songs:[]
+        }
+    },
+    mounted(){
+        console.log("mounted");
+        //Get all songs
+       this.fetchSongs();
+    },
+    methods:{
+        goToPage(page){
+            this.$emit("change-page",page);
+        },
+    fetchSongs(){
+           //get alle the songs
+            const url ="http://webservies.be/eurosong/Songs";
+
+            fetch(url)
+            .then((response)=> {
+                return response.json();
+            })
+            .then((songs)=>
+            {
+               this.songs=songs;
+            console.log(songs);
+            }) 
+        }
+        }}
+
+    
+</script>
