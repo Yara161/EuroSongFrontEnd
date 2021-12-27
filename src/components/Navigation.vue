8722670ee1ffdcf5ff6c1b7fc290d0d5aab1ff0b
Yara161 <66169990+Yara161@users.noreply.github.com>
2021-12-27 10:53:29 +0100

    Add files via upload

---
+++ src/components/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="navbar">
+      <div class="navbar">
+        <button class="navbar button" @click="goToPage('home')">
+            Home
+        </button>
+        <button class="navbar button" @click="goToPage('game')">
+                Game
+        </button>
+        <button class="navbar button active"   @click="goToPage('ranking')">
+            Ranking
+        </button>
+  </div>
+  </div>
+</template>
+<script>
+    export default {
+        name: "Navigation",
+    methods:{
+        //navigation method
+        goToPage(page){
+            this.$emit("change-page",page);
+        }
+    }
+    }
+</script>
